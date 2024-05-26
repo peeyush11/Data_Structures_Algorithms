c07ebecccc05ad913cd52721c611aaba641ac4d7
Peeyush Kumar <peeyushkumar11@gmail.com>
2024-05-27 01:51:29 +0200

    max element search in tree, 3 methods

--- tree/binary_tree_find_max.cpp
+++ tree/binary_tree_find_max.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <queue>
+#include <stack>
+#include <climits>
+#include <algorithm>
 
 struct Node{
     int data_;
@@ -10,26 +13,56 @@ struct Node{
     ~Node(){}
 };
 
-int findMax(Node* temp){
+int FindMaxDfsRecursive(Node* root){
 
-    int root_val, left, right, max = -1;
+    if(root == nullptr) return INT_MIN;
 
-    if(temp != nullptr){
+    int left_max = FindMaxDfsRecursive(root->left_);
+    int right_max = FindMaxDfsRecursive(root->right_);
 
-        root_val = temp->data_;
-        left = findMax(temp->left_);
-        right = findMax(temp->right_);
+    return std::max({root->data_, left_max, right_max}); 
+}
 
-        if(left > right)
-            max = left;
-        else
-            max = right;
-        
-        if(root_val > max)
-            max = root_val;
+int FindMaxDfsIterative(Node* root){
+    // this method is iterative solution for DFS in binary tree.
+    // it is implemented using stack
+    std::stack<Node*> st;
+    st.push(root);
+    Node* current;
+    int max_val = INT_MIN;
+
+    while(!st.empty()){
+
+        current = st.top();
+        if(current->data_ > max_val) max_val = current->data_;
+        st.pop();
+
+        if(current->right_) st.push(current->right_);
+        if(current->left_) st.push(current->left_);
     }
 
-    return max;
+    return max_val;
+}
+
+int FindMaxBfs(Node* root){
+    // this level order traversal in a binary tree.
+    // it is implemented using a queue.
+    std::queue<Node*> q;
+    q.push(root);
+    Node* current;
+    int max_val = INT_MIN;
+
+    while(!q.empty()){
+        current = q.front();
+        if(current->data_ > max_val) max_val = current->data_;
+        q.pop();
+
+        if(current->left_) q.push(current->left_);
+        if(current->right_) q.push(current->right_);
+
+    }
+
+    return max_val;
 }
 
 int main(){
@@ -43,8 +76,12 @@ int main(){
     root->right_->right_ = new Node(15);
 
 
-    int max_val = findMax(root);
-    
-    std::cout<<"max value in binary tree: "<< max_val<<std::endl;
+    int max_val_dfs_recursive = FindMaxDfsRecursive(root);
+    int max_val_dfs_iterative = FindMaxDfsIterative(root);
+    int max_val_bfs = FindMaxBfs(root);
+
+    std::cout<<"DFS recurisve, max value in binary tree: "<< max_val_dfs_recursive<<std::endl;
+    std::cout<<"DFS iterative, max value in binary tree: "<< max_val_dfs_iterative<<std::endl;
+    std::cout<<"BFS iterative, max value in binary tree: "<< max_val_bfs<<std::endl;
     return 0;
 }
