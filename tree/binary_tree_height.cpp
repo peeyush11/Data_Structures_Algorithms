a3e38ae15001ea7a2fd7df7e1d9d9b8319d72618
Peeyush Kumar <peeyushkumar11@gmail.com>
2024-05-30 20:00:02 +0200

    tree height update

--- tree/binary_tree_height.cpp
+++ tree/binary_tree_height.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <queue>
 
 struct Node{
     int data_;
@@ -12,7 +13,7 @@ struct Node{
 
 int TreeHeight(Node* root){
     // base case: null node
-    if(root == NULL) return -1;
+    if(root == NULL) return 0;
     
     // base case: leaf node
     // if(root->left_ == NULL & root->right_ == NULL) return 1;
@@ -20,11 +21,39 @@ int TreeHeight(Node* root){
     int left_height = TreeHeight(root->left_);
     int right_height = TreeHeight(root->right_);
 
+    // std::cout<<left_height<<"\t"<<right_height<<"\n";
     int max_height = std::max({left_height, right_height});
+    // std::cout<<max_height<<"\n";
 
     return max_height + 1;
 
 }
+
+int TreeHeightBFS(Node * root){
+    if(root == NULL) return 0;
+
+    std::queue<Node*> q;
+    q.push(root);
+    int height = 0;
+    
+    while(!q.empty()){
+
+        int level_size = q.size();
+        for(int i= 0; i < level_size; i++){
+            Node* current = q.front();
+            q.pop();
+
+            if(current->left_) q.push(current->left_);
+            if(current->right_) q.push(current->right_);
+        }
+
+        height++;
+    }
+
+    return height;
+
+}
+
 int main(){
 
     Node* case_1 = new Node(5);
@@ -42,6 +71,13 @@ int main(){
     int case_1_height = TreeHeight(case_1);
     int case_2_height = TreeHeight(case_2);
 
-    std::cout<<"Case 1 height: "<<case_1_height<<std::endl;
-    std::cout<<"Case 2 height: "<<case_2_height<<std::endl;
+    std::cout<<"Case 1 Recursive function height: "<<case_1_height<<std::endl;
+    std::cout<<"Case 2 Recursive function height: "<<case_2_height<<std::endl;
+
+    case_1_height = TreeHeightBFS(case_1);
+    case_2_height = TreeHeightBFS(case_2);
+
+    std::cout<<"Case 1 BFS height: "<<case_1_height<<std::endl;
+    std::cout<<"Case 2 BFS height: "<<case_2_height<<std::endl;
+
 }
