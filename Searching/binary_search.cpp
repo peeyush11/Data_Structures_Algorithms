285f35124016ffce0f33bf7f18c0cedd11d9c346
Peeyush Kumar <peeyushkumar11@gmail.com>
2024-06-27 16:30:51 +0200

    binary search

---
+++ Searching/binary_search.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+int BinarySearch(int arr[], int low, int high, int target){
+
+    while(low <= high){
+        int mid = low + (high-low)/2;
+
+        if(arr[mid] == target){
+            return mid;
+        }
+
+        if(arr[mid] < target){
+            low = mid + 1;
+        }
+
+        if(arr[mid] > target){
+            high = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+
+int main(){
+
+    int arr[] = {5,8,10,13,15,17,20,22,25};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int target;
+
+    std::cout<<"Enter a number to search ";
+    std::cin>>target;
+    
+    int result = BinarySearch(arr, 0, n-1, target);
+
+    if(result == -1)
+        std::cout<<"Element not found in array\n";
+    else
+        std::cout<<"Element found at index: "<<result<<"\n";
+
+    return 0;
+}
